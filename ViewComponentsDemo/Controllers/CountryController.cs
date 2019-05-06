cd535ce526f498be20120ba86eba753ac37e9eb8
Wolfgang Ofner <wolfgang.ofner@jmc-software.ch>
2019-05-06 12:50:25 +0200

    added hyrid controller / view component

---
+++ ViewComponentsDemo/Controllers/CountryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using ViewComponentsDemo.Data;
+using ViewComponentsDemo.Models;
+
+namespace ViewComponentsDemo.Controllers
+{
+    [ViewComponent(Name = "HybridComponent")]
+    public class CountryController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public CountryController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Country country)
+        {
+            _countryRepository.AddCountry(country);
+
+            return View("Index", "Home");
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<IEnumerable<Country>>(ViewData,
+                _countryRepository.Countries())
+            };
+        }
+    }
+}
